836db4b2a8daca9b602bc1909e61e0bd2c4e3683
sar <sar@NB-SAR-WIN10>
2021-08-13 09:43:14 +0200

    di

--- app/src/main/java/com/sar/shopaholism/App.kt
+++ app/src/main/java/com/sar/shopaholism/App.kt
@@ -1,6 +1,18 @@
 package com.sar.shopaholism
 
 import android.app.Application
+import com.sar.shopaholism.data.di.dataModules
+import com.sar.shopaholism.domain.di.domainModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+            modules(dataModules + domainModules) //+ presentationModules)
+        }
+    }
 }
