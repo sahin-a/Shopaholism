59fc8801a8ea35f133841addde7a00b13bd0547d
sar <sahin.aral@outlook.de>
2022-06-08 14:58:51 +0200

    refactoring

--- app/src/main/java/com/sar/shopaholism/data/wikipedia/WikipediaClient.kt
+++ app/src/main/java/com/sar/shopaholism/data/wikipedia/WikipediaClient.kt
@@ -1,6 +1,7 @@
 package com.sar.shopaholism.data.wikipedia
 
 import com.sar.shopaholism.data.wikipedia.api.WikipediaApiProvider
+import com.sar.shopaholism.data.wikipedia.dto.SearchResultDto
 import com.sar.shopaholism.domain.entity.WikiPage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
@@ -10,25 +11,22 @@ import kotlinx.coroutines.coroutineScope
 class WikipediaClient(
     private val wikipediaApiProvider: WikipediaApiProvider
 ) {
+    private fun String.addHttps() = this.replaceFirst("^//|http://".toRegex(), "https://")
+
+    private suspend fun getPage(searchResult: SearchResultDto) = coroutineScope {
+        val page = wikipediaApiProvider.getPage(searchResult.title)
+        WikiPage(
+            title = page.title,
+            description = searchResult.description ?: "",
+            thumbnailUrl = searchResult.thumbnail?.url?.addHttps() ?: "",
+            htmlUrl = page.html_url
+        )
+    }
+
     suspend fun getPages(title: String, limit: Int): List<WikiPage> = coroutineScope {
         val searchResultsDto = wikipediaApiProvider.searchPages(title, limit)
-
-        return@coroutineScope searchResultsDto.pages.map { searchResult ->
-            async(Dispatchers.IO) {
-                searchResult.thumbnail?.apply {
-                    if (url.startsWith("//")) {
-                        url = url.replaceFirst("//", "https://")
-                    }
-                }
-                val page = wikipediaApiProvider.getPage(searchResult.title)
-
-                WikiPage(
-                    title = page.title,
-                    description = searchResult.description ?: "",
-                    thumbnailUrl = searchResult.thumbnail?.url ?: "",
-                    htmlUrl = page.html_url
-                )
-            }
-        }.awaitAll()
+        return@coroutineScope searchResultsDto.pages
+            .map { async(Dispatchers.IO) { getPage(it) } }
+            .awaitAll()
     }
 }
